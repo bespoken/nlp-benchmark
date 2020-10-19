10e8877978db826c21f4ca066b180300112e82f9
Moisés Ñañez <moisesnandres@hey.com>
2020-10-19 12:04:56 -0500

    Add initial files for ivr tests

---
+++ src/ivr/process/benchmark-interceptor.js
@@ -0,0 +1,6 @@
+const { Interceptor } = require('bespoken-batch-tester')
+
+class BenchmarkInterceptor extends Interceptor {
+}
+
+module.exports = BenchmarkInterceptor
---
+++ src/ivr/process/benchmark-source.js
@@ -0,0 +1,9 @@
+const { Source } = require('bespoken-batch-tester')
+
+class BenchmarkSource extends Source {
+  async loadAll () {
+
+  }
+}
+
+module.exports = BenchmarkSource
