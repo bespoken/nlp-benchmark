f09d4bb153e3755ef8ea44f15f79208b49a83f35
Moisés Ñañez <moisesnandres@hey.com>
2020-10-20 16:55:42 -0500

    Add function to merge audios

---
+++ src/AudioGenerator.js
@@ -0,0 +1,36 @@
+const axios = require('axios')
+const { Config } = require('bespoken-batch-tester')
+
+class AudioGenerator {
+  static async mergeAudios (prefix, clip) {
+    const ffmpegToken = Config.env('FFMPEG_TOKEN')
+    const encodedPrefix = prefix.toString('base64')
+    const encodedClip = clip.toString('base64')
+    const jsonData = {
+      command: 'ffmpeg -i prefix.mp3 -i clip.mp3 -filter_complex \'[0:0][1:0]concat=n=2:v=0:a=1[out]\' -map \'[out]\' -f wav -',
+      input: {
+        'prefix.mp3': encodedPrefix,
+        'clip.mp3': encodedClip
+      }
+    }
+
+    try {
+      const response = await axios.post('https://ffmpeg.bespoken.io', jsonData, {
+        headers: {
+          'Content-Type': 'application/json',
+          'x-access-token': ffmpegToken
+        },
+        responseType: 'json'
+      })
+
+      const { stderr } = response.data
+      console.log(stderr)
+      return stderr
+    } catch (e) {
+      console.error(e.message)
+      return null
+    }
+  }
+}
+
+module.exports = AudioGenerator
