a2a0311631684dbfafd2648e3621c50647db1754
Moisés Ñañez <moisesnandres@hey.com>
2020-10-23 09:40:46 -0500

    Change virtual device base url for ivr

--- src/AudioGenerator.js
+++ src/AudioGenerator.js
@@ -1,10 +1,12 @@
 const axios = require('axios')
 const { Config } = require('bespoken-batch-tester')
+const TTS = require('./tts')
 
 class AudioGenerator {
   static async mergeAudios (prefix, clip) {
     const ffmpegToken = Config.env('FFMPEG_TOKEN')
-    const encodedPrefix = prefix.toString('base64')
+    const prefixMessage = await TTS.textToSpeechPolly(prefix)
+    const encodedPrefix = prefixMessage.toString('base64')
     const encodedClip = clip.toString('base64')
     const jsonData = {
       command: 'ffmpeg -i prefix.mp3 -i clip.mp3 -filter_complex \'[0:0][1:0]concat=n=2:v=0:a=1[out]\' -map \'[out]\' -f wav -',
