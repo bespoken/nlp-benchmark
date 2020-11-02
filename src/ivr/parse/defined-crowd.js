cc6847d68d4cfa4a5f651147b0a4b8cafd4e0650
Edgar Cruzado <edgar@bespoken.io>
2020-11-02 18:09:23 -0500

    update get data set

--- src/ivr/parse/defined-crowd.js
+++ src/ivr/parse/defined-crowd.js
@@ -1,35 +1,22 @@
 const _ = require('lodash')
 const S3 = require('../../S3')
 const TTS = require('../../tts')
-const fs = require('fs')
 const { ffmpeg, pcmToWav, mergeAudios } = require('./../../helper')
 const parse = require('csv-parse/lib/sync')
 require('dotenv').config()
 
-class DefinedCrowdRecording {
-  async process (includePrefix = false) {
-    const dataset = JSON.parse(await S3.get('proccesed-recordings.json'))
-    for (const row in dataset) {
-      if (includePrefix) {
-        const prefix = `Number: ${parseInt(row + 1)}. Expected Phrase:`
-        const prefixMessage = await TTS.textToSpeechPolly(prefix)
-        console.info(`prefixMessage ${prefix}`)
-        const prefixBuffer = pcmToWav(Buffer.from(prefixMessage, 'base64'))
-        const audio = await S3.get(dataset[row].key)
-        const merged = await mergeAudios(prefixBuffer, audio)
-        const mergedBuffer = Buffer.from(merged, 'base64')
-        // TODO
-        await S3.upload(mergedBuffer, 'test.wav')
-      }
-      console.info(`DONE ${row + 1}`)
-      break
-    }
+class DefinedCrowd {
+  constructor () {
+    this.executionPrefix = Date.now()
   }
 
-  async preProcess () {
-    const sourceFile = process.env.DATASET_PATH
-    const data = fs.readFileSync(sourceFile, 'utf8')
-    const dataset = parse(data, {
+  async getDataset () {
+    return JSON.parse(await S3.get('proccesed-recordings.json', 'ivr-benchmark-defined-crowd'))
+  }
+
+  async process () {
+    const sourceFile = await S3.get('en-us_transcriptions.tsv', 'ivr-benchmark-defined-crowd')
+    const dataset = parse(sourceFile, {
       delimiter: '\t',
       columns: true
     })
@@ -38,7 +25,6 @@ class DefinedCrowdRecording {
       startTime: row.start_time,
       endTime: row.end_time
     })
-    // Generate a dataset combining the transcriptions for the same recordings
     const reducedDataset = dataset.reduce((finalDataset, row) => {
       if (Object.keys(finalDataset).includes(row.recordingid)) {
         finalDataset[row.recordingid].transcripts.push(rowToTranscript(row))
@@ -52,14 +38,17 @@ class DefinedCrowdRecording {
 
     const finalDataset = []
     for (const recordingId in reducedDataset) {
-      const path = `./input/ivr/recordings/${recordingId}.wav`
-      const proccesedRecording = await this.processRecording(path, reducedDataset[recordingId].transcripts)
+      const recordingKey = `recordings/${recordingId}.wav`
+      console.log(`starting processing ${recordingKey}`)
+      const recording = await S3.get(recordingKey, 'ivr-benchmark-defined-crowd')
+      const proccesedRecording = await this.processRecording(recording, reducedDataset[recordingId].transcripts)
       console.log(`starting uploading files ${proccesedRecording.length}`)
+
       for (const i in proccesedRecording) {
-        const key = `${recordingId}-${i}.wav`
+        const key = `processed/${recordingId}-${i}.wav`
         const audioBuffer = Buffer.from(proccesedRecording[i].buffer, 'base64')
         console.log(`uploading ${key}`)
-        await S3.upload(key, audioBuffer)
+        await S3.upload(key, audioBuffer, 'ivr-benchmark-defined-crowd')
         finalDataset.push({
           key,
           transcript: reducedDataset[recordingId].transcripts[i].transcription,
@@ -69,21 +58,20 @@ class DefinedCrowdRecording {
         })
       }
     }
-    await S3.upload('proccesed-recordings.json', JSON.stringify(finalDataset))
-    console.log(finalDataset)
+    console.log('uploading proccesed-recordings.json')
+    await S3.upload('proccesed-recordings.json', JSON.stringify(finalDataset), 'ivr-benchmark-defined-crowd')
   }
 
-  async processRecording (filePath, transcripts) {
-    console.debug(`starting processRecording ${filePath}, transcripts: ${JSON.stringify(transcripts)}`)
+  async processRecording (fileBuffer, transcripts) {
+    console.debug(`starting processRecording transcripts: ${JSON.stringify(transcripts)}`)
     if (!transcripts || transcripts.length === 0) {
       return []
     }
-    const recording = fs.readFileSync(filePath).toString('base64')
 
     const mapChannelPayload = {
       command: 'ffmpeg -i recording.wav -map_channel 0.0.0 -f wav -',
       input: {
-        'recording.wav': recording
+        'recording.wav': fileBuffer
       }
     }
     const results = []
@@ -103,16 +91,33 @@ class DefinedCrowdRecording {
       })
       console.debug('processTranscript completed')
     }
-    console.debug(`completed processRecording ${filePath}`)
     return results
+  }
+
+  async getUrl (proccesedRecord, prefix) {
+    let bucket = 'ivr-benchmark-defined-crowd'
+    let key = proccesedRecord.key
+    if (prefix) {
+      const prefixSsml = `<speak><break time='200ms'/>${prefix}</speak>`
+      const prefixAudio = await TTS.textToSpeechPolly(prefixSsml)
+      const prefixBuffer = pcmToWav(Buffer.from(prefixAudio, 'base64'))
+      const audio = await S3.get(key, 'ivr-benchmark-defined-crowd')
+      const merged = await mergeAudios(prefixBuffer, audio)
+      const mergedBuffer = Buffer.from(merged, 'base64')
+      const fileName = proccesedRecord.key.substr(proccesedRecord.key.lastIndexOf('/') + 1)
+      key = `${this.executionPrefix}-${fileName}`
+      bucket = 'ivr-benchmark-utterances'
+      await S3.upload(key, mergedBuffer)
+    }
+    return S3.getUrl(key, bucket)
   }
 }
 
 module.exports = {
-  DefinedCrowdRecording
+  DefinedCrowd
 }
 
 if (_.nth(process.argv, 2) === 'process') {
-  const recording = new DefinedCrowdRecording()
-  recording.process(true)
+  const recording = new DefinedCrowd()
+  recording.process().then(() => console.log('Done!'))
 }
--- src/ivr/process/benchmark-source.js
+++ src/ivr/process/benchmark-source.js
@@ -1,31 +1,30 @@
 const { Config, Record, Source } = require('bespoken-batch-tester')
-const S3 = require('../../S3')
-const { fetchDataset } = require('../../helper')
+const { DefinedCrowd } = require('../parse/defined-crowd')
 
 class BenchmarkSource extends Source {
   async loadAll () {
     const jobName = Config.get('job')
     const number = Config.get('virtualDeviceConfig.phoneNumber')
-    const dataset = fetchDataset()
+    const definedCrowd = new DefinedCrowd()
+    const dataset = definedCrowd.getDataset()
     const records = []
 
-    for (const row in dataset) {
-      const prefix = `Number: ${dataset[row].index + 1}. Expected Phrase:`
-      const record = new Record(`${prefix} ${dataset[row].fullTranscript}`)
+    for (const i in dataset) {
+      const record = new Record('')
       const platform = jobName.replace('ivr-benchmark-', '')
       if (platform.includes('twilio')) {
-        // TODO: url should not have Number: {number}. Expected Phrase:
-        const recordingUrl = S3.getUrl(row)
+        const recordingUrl = definedCrowd.getUrl(dataset[i])
         record.utterance = recordingUrl
       } else {
-        const recordingUrl = S3.getUrl(row)
+        const prefix = `Number: ${i + 1}. Expected Phrase:`
+        const recordingUrl = definedCrowd.getUrl(dataset[i], prefix)
         record.utterance = recordingUrl
       }
       record.meta = {
         platform: platform,
         number: number,
-        recordingId: row,
-        index: dataset[row].index + 1
+        recordingId: i,
+        index: i + 1
       }
       records.push(record)
     }
