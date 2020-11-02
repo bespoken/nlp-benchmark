e61a1949b32ea75d3fb3b2657324d4a9d88f4fdc
Moisés Ñañez <moisesnandres@hey.com>
2020-11-02 09:57:46 -0500

    Make calls for dialog flow and amazon connect

--- src/ivr/process/benchmark-interceptor.js
+++ src/ivr/process/benchmark-interceptor.js
@@ -1,6 +1,10 @@
 const { Interceptor } = require('bespoken-batch-tester')
 
 class BenchmarkInterceptor extends Interceptor {
+  async interceptRequest (request, device) {
+    request[0].settings = {}
+    request[0].settings.finishOnPhrase = 'say your utterance now'
+  }
 }
 
 module.exports = BenchmarkInterceptor
